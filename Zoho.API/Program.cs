aa9c2ee7bb6a065acb163eb5b3938b3cc3c1cdd6
bhushan.doble <bhushan.doble@zconsolutions.com>
2023-07-06 16:40:50 +0530

    code added

--- Zoho.API/Program.cs
+++ Zoho.API/Program.cs
@@ -36,11 +36,11 @@ builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
